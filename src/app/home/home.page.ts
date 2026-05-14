e24ae70eacfcaf487503f255b086c206d9e74e27
Greyberet <58717769+Greyberet@users.noreply.github.com>
2026-05-13 21:58:34 -0500

    did i cook chat? lmao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation'
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
+  private map!: L.Map;
+
   constructor() {}
+
+  async getPoint() {
+    const position = await Geolocation.getCurrentPosition()
+
+    console.log(position)
+
+    this.map = L.map("map").setView(
+      [
+        position.coords.latitude,
+        position.coords.longitude
+      ],
+      18
+    )
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: "Super Mapa!"
+    }).addTo(this.map);
+
+    const customIcon = L.icon({
+      iconUrl: 'assets/marker.svg',
+      iconSize: L.point(40, 40),
+      iconAnchor: L.point(20, 20)
+    })
+
+    L.marker([position.coords.latitude, position.coords.longitude], {
+      icon: customIcon
+    }).addTo(this.map);
+
+
+  }
 }
